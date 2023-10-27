36fae8adad26f66d48d3e0a39db3f81b94ce3f10
Dayeon Jung <dyjung.dev@gmail.com>
2023-10-27 18:59:17 +0900

    2023-10-27 commited

---
+++ coding-dojang/Practice_9.3.py
@@ -0,0 +1,5 @@
+# s = 'Python is a programming language that lets you work quickly\nand\nintergrate systmes more effectively.'
+s = '''Python is a programming language that lets you work quickly
+and
+intergrate systmes more effectively.'''
+print(s)
